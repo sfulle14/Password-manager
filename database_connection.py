c630a836cb85b7310caee58b54cda4e0090f5570
Steven Fuller <steven.fuller96@gmail.com>
2024-05-08 13:55:09 -0500

    added add password functionality

--- database_connection.py
+++ database_connection.py
@@ -1,9 +1,11 @@
 import sqlite3
 
+# Create the database file
 def create_connection():
     conn = sqlite3.connect('password_manager.db')  # This will create the database file if it doesn't exist
     return conn
 
+# Setup the database by creating the tables
 def setup_database():
     conn = create_connection()
     c = conn.cursor()
@@ -16,6 +18,7 @@ def setup_database():
     conn.commit()
     conn.close()
 
+# Add data to accounts table
 def add_account(website, username, password):
     conn = create_connection()
     c = conn.cursor()
@@ -23,9 +26,20 @@ def add_account(website, username, password):
     conn.commit()
     conn.close()
 
+# Add data to users table
 def add_users(username, password):
     conn = create_connection()
     c = conn.cursor()
     c.execute("INSERT INTO users (username, password) VALUES(?, ?)", (username, password))
     conn.commit()
     conn.close()
+
+# Get count of records in accounts table
+def get_record_count():
+    conn = create_connection()
+    c = conn.cursor()
+    c.execute("SELECT COUNT(*) FROM accounts")
+    count = c.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return count
--- main.py
+++ main.py
@@ -10,13 +10,12 @@ import tkinter as tk
 from tkinter import messagebox
 import database_connection
 
-
 class AppController:
     def __init__(self, root):
         self.root = root
         self.frames = {}
 
-        for F in (LoginApp, PasswordManagerApp):
+        for F in (LoginApp, PasswordManagerApp, AddPassword):
             frame = F(root, self)
             self.frames[F] = frame
             frame.main_frame.grid(row=0, column=0, sticky="nsew")
@@ -48,6 +47,14 @@ class LoginApp:
         # Add username input
         self.user = tk.Entry(self.main_frame, width=25)
         self.user.grid(row=1, column=1, pady=10)  # Place next to the label
+
+        # Add password label
+        self.passwordLabel = tk.Label(self.main_frame, text="Password:", font=("Arial", 14))
+        self.passwordLabel.grid(row=2, column=0, sticky=tk.E)  # Align to the right (East)
+        
+        # Add password input
+        self.password = tk.Entry(self.main_frame, width=25)
+        self.password.grid(row=2, column=1, pady=10)  # Place next to the label
         
         # Add a button to show password
         self.show_button = tk.Button(self.main_frame, text="Login", command=lambda: controller.show_frame(PasswordManagerApp))
@@ -66,6 +73,51 @@ class PasswordManagerApp:
         
         # Add Program label
         self.label = tk.Label(self.main_frame, text="Login worked!", font=("Arial", 14))
+        self.label.grid(row=0, column=0, columnspan=2, pady=10)  # Span across both columns
+
+        # Add password Button
+        self.add_password_button = tk.Button(self.main_frame, text="Add Password", command=lambda: controller.show_frame(AddPassword))
+        self.add_password_button.grid(row=0, column=2)  # Placed at the top right
+
+        # Logout Button
+        self.logout_button = tk.Button(self.main_frame, text="Logout", command=lambda: controller.show_frame(LoginApp))
+        self.logout_button.grid(row=0, column=3, sticky=tk.E)  # Placed at the top right
+
+        for i in range(database_connection.get_record_count()):
+            # Add website label
+            self.label = tk.Label(self.main_frame, text="Websites", font=("Arial", 14))
+            self.label.grid(row=1, column=0, columnspan=2, pady=10)  # Span across both columns
+
+            self.website_entry = tk.Entry(self.main_frame, width=25)
+            self.website_entry.grid(row=2, column=0, columnspan=2, pady=10)
+
+            # Add Username label
+            self.label = tk.Label(self.main_frame, text="Username", font=("Arial", 14))
+            self.label.grid(row=1, column=2, columnspan=2, pady=10)  # Span across both columns
+
+            self.user_entry = tk.Entry(self.main_frame, width=25)
+            self.user_entry.grid(row=2, column=2, columnspan=2, pady=10)
+
+            # Add Password label
+            self.label = tk.Label(self.main_frame, text="Password", font=("Arial", 14))
+            self.label.grid(row=1, column=4, columnspan=2, pady=10)  # Span across both columns
+
+            self.password_entry = tk.Entry(self.main_frame, width=25, show="*")
+            self.password_entry.grid(row=2, column=4, columnspan=2, pady=10)
+
+
+class AddPassword:
+    def __init__(self, root, controller):
+        self.root = root
+        self.controller = controller
+        self.root.title("Password Manager")
+
+        # Create main frame
+        self.main_frame = tk.Frame(self.root, padx=10, pady=10)
+        self.main_frame.grid(row=0, column=0, sticky="nsew")
+
+        # Add Program label
+        self.label = tk.Label(self.main_frame, text="Add Login Data", font=("Arial", 14))
         self.label.grid(row=0, column=0, columnspan=2, pady=10)  # Span across both columns
 
         # Add website label
@@ -89,13 +141,18 @@ class PasswordManagerApp:
         self.password_entry = tk.Entry(self.main_frame, width=25, show="*")
         self.password_entry.grid(row=2, column=4, columnspan=2, pady=10)
 
+        # Add password Button
+        self.add_password_button = tk.Button(self.main_frame, text="Submit", command=self.save_password)
+        self.add_password_button.grid(row=3, column=2, columnspan=2, pady=10)  # Placed at the top right
 
-        def save_password(self):
+    def save_password(self):
             website = self.website_entry.get()
             username = self.user_entry.get()
             password = self.password_entry.get()
             database_connection.add_account(website, username, password)
             messagebox.showinfo("Info", "Password saved successfully!")
+            self.controller.show_frame(PasswordManagerApp)
+    
 
 def main():
     database_connection.setup_database()
