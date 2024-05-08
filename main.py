b3159b1bf28b3e6a058cdd43c4f2f684d5c92b80
Steven Fuller <steven.fuller96@gmail.com>
2024-05-08 12:41:41 -0500

    first

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Project: Password manager
+Author: Steven Fuller
+Start date: May 8, 2024
+Description: The purpose of this project is to create a password manager using modern security concepts.
+This passwords and usernames should be hidden until clicked and linked to a website.
+"""
+
+
+
